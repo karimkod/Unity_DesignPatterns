2fd51f3b0e92f2daa04bb841482727da7659453f
Abdelkrim bournane <ea_bournane@esi.dz>
2019-09-30 14:42:20 +0100

    Added Singleton pattern

---
+++ Project/Assets/Samples/Singleton/Scripts/GameManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : Singleton<GameManager>
+{
+   public void InitGame()
+    {
+        Debug.Log("Game have been initialized.");
+    }
+}
